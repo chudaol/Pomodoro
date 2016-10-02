9ee15e05aba664dc43bf196f15a814b8f7a7cf58
Olga Filipova <chudaol@gmail.com>
2016-10-02 04:21:50 +0200

    conditional working time for testing and non-testing environment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-export const WORKING_TIME = 20 * 60
+let workingTime = process.env.NODE_ENV === 'testing' ? 0.1 * 60 : 20 * 60
+export const WORKING_TIME = workingTime
 export const RESTING_TIME = 5 * 60
 export const KITTEN_TIME = 5
